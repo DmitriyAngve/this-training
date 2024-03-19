7ad9703908060d86616ebf564f7d4eba52ea5619
DmitriyAngve <kirirch129@gmail.com>
2024-03-19 13:15:53 +0300

    #

--- script.js
+++ script.js
@@ -422,4 +422,97 @@ console.log(arr.filter(inBetween(3, 6))); // 3,4,5,6
 console.log(arr.filter(inArray([1, 2, 10]))); // 1,2
 */
 
+/*
 // 23
+function f() {
+  console.log(this); 
+}
+
+let user = {
+  g: f.bind(null),
+};
+
+user.g(); // null
+*/
+
+/*
+// 24
+// Можем ли мы изменить this дополнительным связыванием?
+// Что выведет этот код?
+function f() {
+  console.log(this.name);
+}
+
+f = f.bind({ name: "Вася" }).bind({ name: "Петя" });
+
+f(); // Вася
+*/
+
+/*
+// 25
+// В свойство функции записано значение. Изменится ли оно после применения bind? Обоснуйте ответ.
+function sayHi() {
+  console.log(this.name);
+}
+sayHi.test = 5;
+
+let bound = sayHi.bind({
+  name: "Вася",
+});
+
+console.log(bound.test); // что выведет? почему?
+
+// Ответ: undefined.
+
+// Результатом работы bind является другой объект. У него уже нет свойства test.
+*/
+
+/*
+// 26
+// Вызов askPassword() в приведённом ниже коде должен проверить пароль и затем вызвать user.loginOk/loginFail в зависимости от ответа.
+
+// Однако, его вызов приводит к ошибке. Почему?
+
+// Исправьте выделенную строку, чтобы всё работало (других строк изменять не надо).
+function askPassword(ok, fail) {
+  let password = prompt("Password?", "");
+  if (password == "rockstar") ok();
+  else fail();
+}
+
+let user = {
+  name: "Вася",
+
+  loginOk() {
+    alert(`${this.name} logged in`);
+  },
+
+  loginFail() {
+    alert(`${this.name} failed to log in`);
+  },
+};
+
+askPassword(user.loginOk.bind(user), user.loginFail.bind(user));
+*/
+
+/*
+// 27
+// Объект user был изменён. Теперь вместо двух функций loginOk/loginFail у него есть только одна – user.login(true/false).
+
+// Что нужно передать в вызов функции askPassword в коде ниже, чтобы она могла вызывать функцию user.login(true) как ok и функцию user.login(false) как fail?
+function askPassword(ok, fail) {
+  let password = prompt("Password?", "");
+  if (password == "rockstar") ok();
+  else fail();
+}
+
+let user = {
+  name: "John",
+
+  login(result) {
+    alert(this.name + (result ? " logged in" : " failed to log in"));
+  },
+};
+
+askPassword(user.login.bind(user, true), user.login.bind(user, false));
+*/
